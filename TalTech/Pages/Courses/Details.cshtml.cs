b460e93c106cfeea7fcd427895e1cd6343845ddf
Kristen Niilop <kristen.niilop@gmail.com>
2020-02-09 19:50:43 +0200

    Erroritega project. (indexmodel error). _contextid välja commentitud

---
+++ TalTech/Pages/Courses/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TalTech.Data;
+using TalTech.Models;
+
+namespace TalTech
+{
+    public class DetailsModel : PageModel
+    {
+        //private readonly TalTech.Data.TalTechContext _context;
+
+        public DetailsModel(TalTech.Data.TalTechContext context)
+        {
+            _context = context;
+        }
+
+        public Course Course { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Course = await _context.Courses
+                .Include(c => c.Department).FirstOrDefaultAsync(m => m.CourseID == id);
+
+            if (Course == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ TalTech/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TalTech.Data;
+using TalTech.Models;
+
+namespace TalTech
+{
+    public class IndexModel : PageModel
+    {
+        //private readonly TalTech.Data.TalTechContext _context;
+
+        public IndexModel(TalTech.Data.TalTechContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses
+                .Include(c => c.Department).ToListAsync();
+        }
+    }
+}
