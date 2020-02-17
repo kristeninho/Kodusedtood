0843b94eef9edd9d9e289e92a16ff40e658af68b
Kristen Niilop <kristen.niilop@gmail.com>
2020-02-17 19:16:32 +0200

    enne staff controllerit

---
+++ FCArsenal/Models/FootballViewModels/StaffIndexData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FCArsenal.Models.SchoolViewModels
+{
+    public class StaffIndexData
+    {
+        public IEnumerable<Staff> Staffs { get; set; }
+        public IEnumerable<Training> Trainings { get; set; }
+        public IEnumerable<Signing> Signings { get; set; }
+    }
+}
---
+++ FCArsenal/Views/Trainings/TrainingsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FCArsenal.Data;
+using FCArsenal.Models;
+
+namespace FCArsenal.Views.Trainings
+{
+    public class TrainingsController : Controller
+    {
+        private readonly FootballContext _context;
+
+        public TrainingsController(FootballContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Trainings
+        public async Task<IActionResult> Index()
+        {
+            var footballContext = _context.Trainings.Include(t => t.Department);
+            return View(await footballContext.ToListAsync());
+        }
+
+        // GET: Trainings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var training = await _context.Trainings
+                .Include(t => t.Department)
+                .FirstOrDefaultAsync(m => m.TrainingID == id);
+            if (training == null)
+            {
+                return NotFound();
+            }
+
+            return View(training);
+        }
+
+        // GET: Trainings/Create
+        public IActionResult Create()
+        {
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+            return View();
+        }
+
+        // POST: Trainings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TrainingID,Title,Credits,DepartmentID")] Training training)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(training);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID", training.DepartmentID);
+            return View(training);
+        }
+
+        // GET: Trainings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var training = await _context.Trainings.FindAsync(id);
+            if (training == null)
+            {
+                return NotFound();
+            }
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID", training.DepartmentID);
+            return View(training);
+        }
+
+        // POST: Trainings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TrainingID,Title,Credits,DepartmentID")] Training training)
+        {
+            if (id != training.TrainingID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(training);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TrainingExists(training.TrainingID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID", training.DepartmentID);
+            return View(training);
+        }
+
+        // GET: Trainings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var training = await _context.Trainings
+                .Include(t => t.Department)
+                .FirstOrDefaultAsync(m => m.TrainingID == id);
+            if (training == null)
+            {
+                return NotFound();
+            }
+
+            return View(training);
+        }
+
+        // POST: Trainings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var training = await _context.Trainings.FindAsync(id);
+            _context.Trainings.Remove(training);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TrainingExists(int id)
+        {
+            return _context.Trainings.Any(e => e.TrainingID == id);
+        }
+    }
+}
